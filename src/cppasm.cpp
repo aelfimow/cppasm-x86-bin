5f36a4f069bf4780355a42ad179fcc757ad5f8c0
Alexander Elfimow <my.free.mail@gmx.de>
2020-01-05 21:11:06 +0100

    Added files

---
+++ src/cppasm.cpp
@@ -0,0 +1,6 @@
+/*! \file cppasm.cpp
+    \brief Main file of the C++ Assembly Generator.
+*/
+
+#include "cppasm.h"
+
---
+++ src/cppasm.h
@@ -0,0 +1,16 @@
+/*! \file cppasm.h
+    \brief Main header file of the C++ Assembly Generator.
+*/
+#ifdef CPPASM_H
+#error Already included
+#else
+#define CPPASM_H
+
+#include <string>
+#include <string_view>
+#include <vector>
+#include <initializer_list>
+
+#include "OperandEncoding.h"
+
+#endif
