c32a7d31ac5bad36a19d14cd353165f259d3ea6e
Alexander Elfimow <my.free.mail@gmx.de>
2020-01-08 20:38:46 +0100

    Added header file

---
+++ src/cppasm_types.h
@@ -0,0 +1,18 @@
+/*! \file cppasm_types.h
+    \brief cppasm specific types.
+*/
+#ifdef CPPASM_TYPES_H
+#error Already included
+#else
+#define CPPASM_TYPES_H
+
+#include <cstdint>
+#include <string_view>
+
+/*! Operation code type  */
+using opcode_t = int;
+
+/*! Comment strings type  */
+using comment_t = std::string_view;
+
+#endif
